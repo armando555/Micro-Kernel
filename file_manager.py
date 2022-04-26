bc3321021f791fb106b6078a9b873090e6d939b1
Armando Rios Gallego <arios@coreplus.net>
2022-04-25 23:10:05 -0500

    module of file manager implemented

--- file_manager.py
+++ file_manager.py
@@ -1,4 +1,6 @@
+import json
 import socket
+import os
 host = "localhost"
 port = 5001
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -9,8 +11,18 @@ active_connection, address = server.accept()
 
 while(True):
     recibido = active_connection.recv(1024)
-    print("Client answer: "+recibido.decode(encoding="ascii", errors="ignore"))
-    enviar = input("Server: ")
+    message = json.loads(recibido.decode(encoding="ascii", errors="ignore"))
+    error = 1
+    if(message["action"]=="1"):
+        error = os.system("mkdir "+message["value"])
+    else:
+        error = os.system("touch "+message["value"])
+    print("Orden: "+recibido.decode(encoding="ascii", errors="ignore"))
+    if(error != 0):
+        enviar = "Ocurrio un error al ejecutar la acción de "+("crear carpeta" if message["action"] == "1" else "crear archivo")
+    else :
+        enviar = "Accion realizada : "+("crear carpeta" if message["action"] == "1" else "crear archivo")
     active_connection.send(enviar.encode(encoding="ascii",errors="ignore"))
+    
 
 active_connection.close()
--- kernel.py
+++ kernel.py
@@ -1,12 +1,18 @@
 import socket
+import json
 host = "localhost"
 port = 5001
 objsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 objsocket.connect((host,port))
-print("iniciamos cliente")
+print("starting client")
 
 while(True):
-    enviar = input("Client: ")
+    message = {"action":"","value":""} 
+    operation = input("Ingrese operacion\n")
+    value = input("Ingrese el valor\n")
+    message["action"] = operation
+    message["value"] = value
+    enviar = json.dumps(message)
     objsocket.send(enviar.encode(encoding="ascii", errors="ignore"))
     recibido = objsocket.recv(1024)
     print("Servidor: "+recibido.decode(encoding="ascii", errors="ignore"))
