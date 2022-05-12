ee1c9a7a55bfeb055c23118a6497659e53621534
Juan Diego Mejía <juan.mejia@pagame.com.co>
2022-05-12 07:31:01 -0500

    Creación de archivo finalizada

--- front/src/App.js
+++ front/src/App.js
@@ -12,8 +12,16 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
+      logs:[],
       btnDirectorio: false,
-      directorio: ""
+      directorio: "",
+      btnArchivo: false,
+      archivo:"",
+      styleSheet: {
+        btnCrearDirectorio :{
+          margin: "2vh"
+        }
+      }
     }
   }
 
@@ -23,19 +31,30 @@ class App extends Component {
       console.log('WebSocket Client Connected');
     };
     client.onmessage = (message) => {
-      console.log(message);
+
+      console.log(message.data);
     };
+    client.addEventListener('message', (event)=>{
+      this.setState({logs: [...this.state.logs,event.data]})
+      
+    })
+    
   }
 
+  
+
+
   render() {
+  
     return (
       <div style={{
         display: "flex",
         flexDirection: "column",
         alignContent:"center",
-        alignItems:"center"
+        alignItems:"center",
+
       }}>
-        <button onClick={() => {
+        <button style={this.state.styleSheet.btnCrearDirectorio} onClick={() => {
           this.setState({btnDirectorio: !this.state.btnDirectorio})
           
         }}>{this.state.btnDirectorio ? <>Cerrar</> :<>Crear directorio</>}</button>
@@ -46,20 +65,51 @@ class App extends Component {
               const data = {
                 cmd: "1",
                 msg: this.state.directorio,
-                scr: "abueno2",
-                dst: "destino :V"
+                scr: "gui-user",
+                dst: "file_manager",
               }
-              console.log(data)
-              client.send(JSON.stringify(data))
-              
+              client.send(JSON.stringify(data))              
             }}>Crear</button>
+
+            
+
+
           </div>
         ):<></>}
 
 
-        <button>
+        <button style={this.state.styleSheet.btnCrearDirectorio} onClick={()=>{
+          this.setState({btnArchivo: !this.state.btnArchivo})
+        }}>
           crear Archiivo
         </button>
+
+        {this.state.btnArchivo ? (
+          <div>
+            <input placeholder="Nombre" value={this.state.archivo} onChange= {(e)=>{(this.setState({archivo:e.target.value}))}}></input>
+            <button onClick={() =>{
+              const data = {
+                cmd: "2",
+                msg: this.state.archivo,
+                scr: "gui-user",
+                dst: "file_manager",
+              }
+              console.log(data)
+              client.send(JSON.stringify(data))              
+            }}>Crear</button>
+
+
+          </div>
+        ):<></>}
+
+        <ul>
+        
+        {
+          this.state.logs?.map(item => <li>{item}</li>)
+        }
+
+        </ul>
+
       </div>
     );
   }
