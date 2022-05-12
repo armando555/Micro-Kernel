66ef7434a4d05e03a203b8cb55d960264bf647ff
Juan Diego Mejía <jdmejiav@eafit.edu.co>
2022-05-11 23:10:39 -0500

    Cambios en la creación de archivos y conexión entre kernel y front

--- gui-user.py
+++ gui-user.py
@@ -2,31 +2,25 @@ import json
 import socket
 import os
 from datetime import datetime
+import asyncio
+import websockets
 
 
-host = "localhost"
-port = 5003
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind((host,port))
-server.listen(1)
-print("Server GUI-USER is running waiting for connections")
-active_connection, address = server.accept()
+async def echo(websocket):
+    async for message in websocket:
+        print("gola ejej")
+        print(message)
+        handleConnection(message)
+        await websocket.send(message)
 
-while(True):
-    recibido = active_connection.recv(1024)
-    message = json.loads(recibido.decode(encoding="ascii", errors="ignore"))
-    error = 1
-    if(message["msg"]=="off"):
-        break
-    if(message["cmd"] == "start" and message["msg"]=="all"):
-        print("GUI USER STARTED")
-        error = 0
-    message = {"cmd":"","msg":"","src":"","dst":""} 
-    operation = input("Ingrese operacion\n")
-    msg = input("Ingrese el valor\n")
-    message["cmd"] = operation
-    message["msg"] = msg
-    message["src"] = "gui_user"
+async def main():
+    async with websockets.serve(echo, "localhost", 7777):
+        await asyncio.Future()  # run forever
+
+
+def handleConnection(recv):
+    message = json.loads(recv)
+    
     if(message["cmd"] == "1" or message["cmd"] == "2"):
         message["dst"] = "file_manager"
         enviar = json.dumps(message)
@@ -39,6 +33,28 @@ while(True):
     if(message["cmd"]=="send"):
         enviar = json.dumps(message)
         active_connection.send(enviar.encode(encoding="ascii",errors="ignore"))
-    
 
-active_connection.close()
+
+
+if __name__=='__main__':
+    host = "localhost"
+    port = 5003
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind((host,port))
+    server.listen(1)
+    print("Server GUI-USER is running waiting for connections")
+    active_connection, address = server.accept()
+
+    recibido = active_connection.recv(1024)
+
+    message = json.loads(recibido.decode(encoding="ascii", errors="ignore"))
+    error = 1
+    if(message["msg"]=="off"):
+        print("Ejecución finalizada")
+        os.exit(1)
+    if(message["cmd"] == "start" and message["msg"]=="all"):
+        print("GUI USER STARTED")
+        error = 0
+
+    asyncio.run(main())
+
