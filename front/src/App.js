22c139a0329a71700b67fb21ba5de48e0f133fde
Juan Diego Mejía <juan.mejia@pagame.com.co>
2022-05-23 20:16:02 -0500

    Cambios visuales

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-
-
+import Select from "react-select"
+import "./App.css";
 
 const client = new W3CWebSocket('ws://127.0.0.1:7777');
 
@@ -9,16 +9,18 @@ const client = new W3CWebSocket('ws://127.0.0.1:7777');
 
 class App extends Component {
 
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      logs:[],
+      logs: [],
       btnDirectorio: false,
       directorio: "",
       btnArchivo: false,
-      archivo:"",
+      archivo: "",
+      btnEjecturar: false,
+      ejecutar: "",
       styleSheet: {
-        btnCrearDirectorio :{
+        btnCrearDirectorio: {
           margin: "2vh"
         }
       }
@@ -34,60 +36,81 @@ class App extends Component {
 
       console.log(message.data);
     };
-    client.addEventListener('message', (event)=>{
-      this.setState({logs: [...this.state.logs,event.data]})
-      
+    client.addEventListener('message', (event) => {
+      this.setState({ logs: [...this.state.logs, event.data] })
+
     })
-    
+
   }
 
-  
+
 
 
   render() {
-  
+
+    const options = [
+      { value: 'calc', label: 'Calculadora' },
+    ]
+
+    const handleSelectOption = (selected) => {
+      this.setState({ ejecutar: selected })
+    }
+
     return (
+
       <div style={{
+        height: "100vh",
         display: "flex",
         flexDirection: "column",
-        alignContent:"center",
-        alignItems:"center",
+        alignContent: "center",
+        alignItems: "center",
+        justifyContent: "center"
 
       }}>
         <button style={this.state.styleSheet.btnCrearDirectorio} onClick={() => {
-          this.setState({btnDirectorio: !this.state.btnDirectorio})
-          
-        }}>{this.state.btnDirectorio ? <>Cerrar</> :<>Crear directorio</>}</button>
+          this.setState({ btnDirectorio: !this.state.btnDirectorio })
+
+        }}>{this.state.btnDirectorio ? <>Cerrar</> : <>Crear directorio</>}</button>
         {this.state.btnDirectorio ? (
-          <div>
-            <input placeholder="Nombre" value={this.state.directorio} onChange= {(e)=>{(this.setState({directorio:e.target.value}))}}></input>
-            <button onClick={() =>{
+          <div style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.8rem",
+          }}>
+
+
+            <input placeholder="Nombre" value={this.state.directorio} onChange={(e) => { (this.setState({ directorio: e.target.value })) }}></input>
+            <button onClick={() => {
               const data = {
                 cmd: "1",
                 msg: this.state.directorio,
                 scr: "gui-user",
                 dst: "file_manager",
               }
-              client.send(JSON.stringify(data))              
+              client.send(JSON.stringify(data))
             }}>Crear</button>
 
-            
+
 
 
           </div>
-        ):<></>}
+        ) : <></>}
 
 
-        <button style={this.state.styleSheet.btnCrearDirectorio} onClick={()=>{
-          this.setState({btnArchivo: !this.state.btnArchivo})
+        <button style={this.state.styleSheet.btnCrearDirectorio} onClick={() => {
+          this.setState({ btnArchivo: !this.state.btnArchivo })
         }}>
-          crear Archiivo
+          {this.state.btnArchivo ? <>Cerrar</> : <>Crear Archivo</>}
         </button>
 
         {this.state.btnArchivo ? (
-          <div>
-            <input placeholder="Nombre" value={this.state.archivo} onChange= {(e)=>{(this.setState({archivo:e.target.value}))}}></input>
-            <button onClick={() =>{
+          <div style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.8rem",
+          }}>
+            <input placeholder="Nombre" value={this.state.archivo} onChange={(e) => { (this.setState({ archivo: e.target.value })) }}></input>
+            <button onClick={() => {
               const data = {
                 cmd: "2",
                 msg: this.state.archivo,
@@ -95,22 +118,29 @@ class App extends Component {
                 dst: "file_manager",
               }
               console.log(data)
-              client.send(JSON.stringify(data))              
+              client.send(JSON.stringify(data))
             }}>Crear</button>
 
 
           </div>
-        ):<></>}
+        ) : <></>}
+
+        <button style={this.state.styleSheet.btnCrearDirectorio} onClick={() => {
+          this.setState({ btnEjecturar: !this.state.btnEjecturar })
+        }}>{this.state.btnEjecturar ? <>Cerrar</> : <>Ejecutar aplicación</>}</button>
+        {this.state.btnEjecturar ? <Select options={options} onChange={this.handleSelectOption} /> : <></>}
+
 
         <ul>
-        
-        {
-          this.state.logs?.map(item => <li>{item}</li>)
-        }
+
+          {
+            this.state.logs?.map(item => <li>{item}</li>)
+          }
 
         </ul>
 
       </div>
+
     );
   }
 
